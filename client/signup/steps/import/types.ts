c3521db5cd15633f945aa9e380426dff0d61dc85
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-11-03 18:20:12 +0100

    Import ready: translate feature list per platform

--- client/signup/steps/import/types.ts
+++ client/signup/steps/import/types.ts
@@ -1,1 +1,20 @@
 export type GoToStep = ( stepName: string, stepSectionName?: string ) => void;
+
+export type FeatureName =
+	| 'tags'
+	| 'posts'
+	| 'pages'
+	| 'pages_static'
+	| 'blocks'
+	| 'images'
+	| 'photos'
+	| 'videos'
+	| 'files'
+	| 'styles'
+	| 'themes'
+	| 'themes_custom'
+	| 'colors'
+	| 'fonts'
+	| 'plugins';
+
+export type FeatureList = { [ key in FeatureName ]: string };
